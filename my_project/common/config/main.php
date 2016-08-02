6c6803dd1423db11d627ce3b528f93060882e454
sakeerin <theblue0800@gmail.com>
2016-08-02 13:26:53 +0700

    modify file config

--- my_project/common/config/main.php
+++ my_project/common/config/main.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    // ใส่ชื่อระบบที่เราต้องการไม่เกิดผลกระทบทั้ง backend และ frontend โดยใช้คำสั่ง 'name' => 'ชื่อระบบของเรา'
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
